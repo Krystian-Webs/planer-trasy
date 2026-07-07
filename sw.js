6b5411dd3c69d4b1cd83de1c351994865e594922
Krystian-Webs <ddcompany.kontakt@gmail.com>
2026-07-07 09:41:26 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker dla Planera Trasy — instalacja + offline, z automatyczną aktualizacją.
-const CACHE = "planer-trasy-v14";
+const CACHE = "planer-trasy-v15";
 const SHELL = [
   "./",
   "index.html",
